117fa713ebd6bcf4136e05282162e01f40774643
ibrahimkemalkoyuncu <ibrahimkemalkoyuncu@gmail.com>
2023-03-17 08:42:28 +0300

    validation 1

--- src/app/forms/reactiveform-form-builder/reactiveform-form-builder.component.ts
+++ src/app/forms/reactiveform-form-builder/reactiveform-form-builder.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CountryDropMenu } from 'src/app/models/country-drop-menu';
 import { GenderRadioMenu } from 'src/app/models/gender-radio-menu';
 
@@ -8,7 +8,7 @@ import { GenderRadioMenu } from 'src/app/models/gender-radio-menu';
   templateUrl: './reactiveform-form-builder.component.html',
   styleUrls: ['./reactiveform-form-builder.component.css']
 })
-export class ReactiveformFormBuilderComponent {
+export class ReactiveformFormBuilderComponent implements OnInit{
   loginForm:FormGroup;
   countryMenu: CountryDropMenu[] = [{text:"Seçiniz",value:0},{text:"Türkiye",value:1},{text:"Almanya",value:2},{text:"Fransa",value:3}];
   genderMenu: GenderRadioMenu[] = [{text:"Erkek",value:1},{text:"Kadın",value:2}];
@@ -17,13 +17,16 @@ export class ReactiveformFormBuilderComponent {
 
     this.loginForm = fb.group({
 
-      userName:'', //FormControl
-      password:'',
+      userName: fb.control('',Validators.required), //FormControl
+      password: fb.control('',[Validators.required,Validators.minLength(4)]),
       rememberMe: false,
       country:0,
       gender:[1]
 
     })
+  }
+  ngOnInit(): void {
+    this.loginForm.get("userName")?.valueChanges;
   }
 
 
